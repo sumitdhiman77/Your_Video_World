ba642139c64e52c78dcfe7f64d364ea0cadff237
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-24 18:11:21 +0530

    change

--- src/app/videoUpload/page.tsx
+++ src/app/videoUpload/page.tsx
@@ -65,7 +65,7 @@ function VideoUploadForm() {
               type="text"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              className="text-black w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
             />
           </div>
 
@@ -77,7 +77,7 @@ function VideoUploadForm() {
               rows={3}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              className="text-black w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
             />
           </div>
 
